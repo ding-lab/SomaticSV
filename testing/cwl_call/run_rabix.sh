a3876f84ef117268e8dc8c7e237ea1cfbb33d5bf
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-10-10 16:11:12 -0500

    Successful run of C3L-00079 on Rabix on denali
    * Successful comparison against previous run on MGI

--- testing/cwl_call/run_rabix.sh
+++ testing/cwl_call/run_rabix.sh
@@ -1,5 +1,6 @@
 CWL="../../cwl/somatic_sv_workflow.cwl"
-YAML="project_config.yaml"
+#YAML="project_config.yaml"
+YAML="project_config.C3L-00079.yaml"
 
 mkdir -p results
 RABIX_ARGS="--basedir results"
