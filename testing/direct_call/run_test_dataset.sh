8065881e4d73e49ac808b506f676a3c413fd7ded
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-10-10 06:35:25 +0000

    Bugfix

--- testing/direct_call/run_test_dataset.sh
+++ testing/direct_call/run_test_dataset.sh
@@ -1,4 +1,4 @@
-cd ..
+cd ../..
 
 TUMOR="testing/demo_data/G15512.HCC1954.1.COST16011_region.bam"
 NORMAL="testing/demo_data/HCC1954.NORMAL.30x.compare.COST16011_region.bam"
@@ -20,6 +20,7 @@ fi
 #   --region=8:107652000-107655000 --region=11:94974000-94989000 --candidateBins=4 --exome "
 # These were obtained from :
 #	python /opt/conda/share/manta-1.4.0-1/bin/runMantaWorkflowDemo.py
-ARGS="-T"
+# Test goes much faster with this argument, but yields same result
+$ARGS="-T"
 
 bash process_sample.sh $ARGS -o $OUTD $TUMOR $NORMAL $REF
